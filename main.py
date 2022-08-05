71f5990b9135aaa3b2ec78daf25510db6b05e482
Lucas Cantoni <lukcnt@outlook.com>
2022-08-05 11:20:44 -0300

    Added the random computer choice of rock, paper or scissors.

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import random
+
 # Begining of the game.
 # Instead of let the user write "rock", "paper" or "scissors" I choose
 # represent each one with the numbers "0", "1" and "2", that way I believe
 # that is easier for the user and also lowers the chance of they making a
 # mistake while writing.
 user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors: ")
+
+# Randomly select the computer option.
+possibilities = ["Rock", "Paper", "Scissors"]
+computer_choice = random.choice(possibilities)
